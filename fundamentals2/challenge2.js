6cd90222ba51f4a920e30d5a1dc4e5e497e78c3c
lucymutunga <lucyalphonce18@gmail.com>
2024-03-11 12:35:56 +0300

    adding files

--- fundamentals2/challenge2.js
+++ fundamentals2/challenge2.js
@@ -1,5 +1,16 @@
-MarksWeight = 78;
-MarksHeight = 1.69;
-JohnsWeight = 92;
-JohnsHeight = 1.95;
-MarksBMI = MarksWeight / MarksHeight ** 2;
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+const doAverage = calcAverage(85, 54, 71);
+const koAverage = calcAverage(23, 34, 27);
+console.log(doAverage, koAverage);
+//checkWinner
+
+function checkWinner(koAverage, doAverage) {
+  if (koAverage >= 2 * doAverage) {
+    console.log(`Koalas win (${koAverage} vs. ${doAverage})`);
+  } else if (doAverage >= 2 * koAverage) {
+    console.log(`Dolphins win (${doAverage} vs ${koAverage})`);
+  } else {
+    console.log(`No team wins...`);
+  }
+}
+checkWinner(koAverage, doAverage);
