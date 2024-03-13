fd9d5df5d8f287e4c5e57544ba0415579409c7d2
lucymutunga <lucyalphonce18@gmail.com>
2024-03-13 22:35:29 +0300

    adding challlenge file

---
+++ fundamentals2/challenge4.js
@@ -0,0 +1,28 @@
+const Mark = {
+  fullname: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+const John = {
+  fullname: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+Mark.calcBMI();
+John.calcBMI();
+console.log(John.bmi, Mark.bmi);
+Mark.bmi > John.bmi
+  ? console.log(
+      `${Mark.fullname}'s BMI (${Mark.bmi}) is higher than ${John.fullname}(${John.bmi})`
+    )
+  : console.log(
+      `${John.fullname}'s BMI (${John.bmi}) is higher than ${Mark.fullname}(${Mark.bmi})`
+    );
