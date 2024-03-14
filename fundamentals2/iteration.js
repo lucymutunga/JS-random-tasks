cae74c24c746cd97ce446d4f95148662d0bd4117
lucymutunga <lucyalphonce18@gmail.com>
2024-03-14 22:50:22 +0300

    adding some methods

--- fundamentals2/iteration.js
+++ fundamentals2/iteration.js
@@ -26,3 +26,19 @@ for (let i = 0; i < years.length; i++) {
   ages.push(2037 - years[i]);
 }
 console.log(ages);
+
+//continue and break
+console.log("---ONLY STRINGS---");
+for (let i = 0; i < jonasArray.length; i++) {
+  if (typeof jonasArray[i] !== "string") continue;
+  {
+    console.log(jonasArray[i], typeof jonasArray[i]);
+  }
+}
+console.log("---ONLY NUMBERS---");
+for (let i = 0; i < jonasArray.length; i++) {
+  if (typeof jonasArray[i] === "number") break;
+  {
+    console.log(jonasArray[i], typeof jonasArray[i]);
+  }
+}
