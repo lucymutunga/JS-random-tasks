b870feb5cb82ae6cf33208ff37f66f89cd518282
lucymutunga <lucyalphonce18@gmail.com>
2024-03-15 20:14:44 +0300

    adding files

---
+++ fundamentals2/while.js
@@ -0,0 +1,9 @@
+//The while loop
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting Weights repetition${rep}🏋️‍♂️`);
+// }
+let rep = 1;
+while (rep <= 10) {
+  console.log(`Lifting Weights repetition ${rep}🏋️‍♂️`);
+  rep++;
+}
