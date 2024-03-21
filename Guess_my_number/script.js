a1d563ae9c08bd2ed68a6b942de9b9de9c986686
lucymutunga <lucyalphonce18@gmail.com>
2024-03-21 13:28:37 +0300

    manipulating the dome

--- Guess_my_number/script.js
+++ Guess_my_number/script.js
@@ -1,2 +1,8 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'correct Number💃';
+document.querySelector('.message').textContent = 13;
+document.querySelector('.number').textContent = 26;
+document.querySelector('.score').textContent = 50;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
