9d8a8d04c5340fe9c05b198e4aa9ef3a9781f399
lucymutunga <lucyalphonce18@gmail.com>
2024-03-21 14:10:46 +0300

    adding files

--- Guess_my_number/script.js
+++ Guess_my_number/script.js
@@ -5,4 +5,11 @@ document.querySelector('.message').textContent = 13;
 document.querySelector('.number').textContent = 26;
 document.querySelector('.score').textContent = 50;
 document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// console.log(document.querySelector('.guess').value);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number🤷‍♂️';
+  }
+});
