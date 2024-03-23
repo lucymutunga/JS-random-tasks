520dbfe300c2d06cba598ed8e3a106679569efda
lucymutunga <lucyalphonce18@gmail.com>
2024-03-23 21:48:04 +0300

    changing  screen color for the winner

--- Guess_my_number/script.js
+++ Guess_my_number/script.js
@@ -12,10 +12,15 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+  //if player doesnt win
   if (!guess) {
     document.querySelector('.message').textContent = 'No number🤷‍♂️';
+    //when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'correct  Number💃';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    //when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high📈';
@@ -25,6 +30,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game😭';
       document.querySelector('.score').textContent = 0;
     }
+    //when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low📉';
