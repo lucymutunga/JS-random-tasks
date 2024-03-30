f429513d27fba1bfcca34205397bd825a36dd028
lucymutunga <lucyalphonce18@gmail.com>
2024-03-30 22:01:42 +0300

    adding highscore logic

--- Guess_my_number/script.js
+++ Guess_my_number/script.js
@@ -8,6 +8,7 @@
 // console.log(document.querySelector('.guess').value);
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -21,6 +22,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
     //when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
