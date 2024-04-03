094c877746b2c810d42cd09cb2b71bce6c247cc7
lucymutunga <lucyalphonce18@gmail.com>
2024-04-03 22:42:47 +0300

    adding content

--- fundamentals2/fdec.js
+++ fundamentals2/fdec.js
@@ -10,6 +10,7 @@ calAge(2001);
 console.log(calAge(2001));
 
 //Function expreesion is a function without a name,we use a variable to store the function
+//expressions are values thats why we store it in a variable
 
 const calcAge2 = function (birthyear) {
   return 2037 - birthyear;
@@ -17,3 +18,7 @@ const calcAge2 = function (birthyear) {
 // console.log(calcAge2(2001));
 const age2 = calcAge2(2001);
 console.log(age2);
+
+//difference between function declaration and function expression
+//function declaration can be called before it is defined while function expression cannot be called before it is defined
+//function declaration is hoisted while function expression is not hoisted
