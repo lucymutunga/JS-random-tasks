6f68acf11332328e9cd51e470781a02c4acaf2d0
lucymutunga <lucyalphonce18@gmail.com>
2024-04-08 22:47:40 +0300

    manipulating the modals

--- modalwindow/script.js
+++ modalwindow/script.js
@@ -6,4 +6,18 @@ const btnsOpenModal = document.querySelector(".show-modal");
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].assEventListener("click", function () {
+    console.log("Button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+overlay.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
